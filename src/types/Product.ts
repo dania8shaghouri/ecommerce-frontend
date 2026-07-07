1c64f8df0d72afa099a11e4bbd340f6c4a70d3ee
dania <daniashaghouri@gmail.com>
2026-07-07 13:59:07 +0300

    product types guncellendi

--- src/types/Product.ts
+++ src/types/Product.ts
@@ -1,15 +1,46 @@
 export interface Product {
   _id: string;
+
   title: string;
   brand: string;
+  category: string;
+
   image: string;
   images: string[];
-  cpu: string;
-  ram: string;
-  storage: string;
-  gpu: string;
-  description: string;
+
+  description?: string;
+
+  // Laptop
+  cpu?: string;
+  ram?: string;
+  storage?: string;
+  gpu?: string;
+
+  // Monitor
+  resolution?: string;
+  refreshRate?: string;
+  panel?: string;
+  size?: string;
+
+  // Gaming Products
+  type?: string;
+  connectivity?: string;
+  switches?: string;
+  dpi?: string;
+  rgb?: boolean;
+
+  // Storage
+  capacity?: string;
+  interface?: string;
+  readSpeed?: string;
+
   price: number;
+  stock: number;
+
+  rating: number;
+  reviewCount: number;
+
+  isFeatured?: boolean;
 }
 
 export interface TopProduct {
@@ -17,4 +48,11 @@ export interface TopProduct {
   sales: number;
   revenue: string;
   image: string;
+}
+
+export interface ProductsResponse {
+  products: Product[];
+  totalProducts: number;
+  totalPages: number;
+  currentPage: number;
 }
